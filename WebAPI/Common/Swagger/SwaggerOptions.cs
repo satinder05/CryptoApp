dd92405d14a7e4cd68c8509b055a863f0dbdd3ae
Satinders <satinder.s@makeitmine.com.au>
2020-05-07 23:22:12 +1000

    rip api

--- WebAPI/Common/Swagger/SwaggerOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAPI.Common.Swagger
-{
-    public class SwaggerOptions
-    {
-        public string JsonRoute { get; set; }
-        public string Description { get; set; }
-        public string UiEndPoint { get; set; }
-    }
-}
